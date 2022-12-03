88aa3ec819266a16f106cb16eb4f3110348038bd
Vivek Kumar <vivek.26@outlook.com>
2022-12-03 12:13:15 +0530

    personalize gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Vivek Kumar',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Vivek Kumar is a software engineer who specializes in designing and building robust backend systems.',
+    siteUrl: 'https://vivek.sh', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@vkmr26',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'VivekKumar',
+        short_name: 'VivekKumar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
@@ -149,6 +149,7 @@ module.exports = {
         ],
       },
     },
+    // TODO: change this
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
